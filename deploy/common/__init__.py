96e0940cb360a1b91d4575d588c5b9f94b15365f
ekaterina <mozhegova04@gmail.com>
2026-01-30 17:23:00 +0300

    refactor

--- deploy/common/__init__.py
+++ deploy/common/__init__.py
@@ -2,7 +2,7 @@ from .joint_mapping import ISAAC_TO_MUJOCO, MUJOCO_TO_ISAAC
 from .policy_loader import load_policy, RawPolicyWrapper, JitPolicyWrapper
 from .observation import Observation
 from .action import Action
-from .commander import Commander
+from .command_manager import CommandManager
 from .rl_controller import RLController
 
 __all__ = [
